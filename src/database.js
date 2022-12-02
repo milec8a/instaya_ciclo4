7ef8c304272fcc7e74cd1b4fa4900d53394ab129
milec8a <113398003+milec8a@users.noreply.github.com>
2022-12-02 18:52:14 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const URI = 'mongodb+srv://admin_instaya:instaya5@instaya5.fqsqwis.mongodb.net/?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://<usuario>:<clave>@instaya5.fqsqwis.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(URI)
 .then(()=> { console.log('DB connected')})
 .catch(()=> { console.log('DB connected')})
